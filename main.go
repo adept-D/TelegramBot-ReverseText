9ee598b222348a8293e1ae46d4f0b4f074f9cc32
CyberAdept <diasminibaev@bk.ru>
2021-09-25 01:36:05 +0400

    Create TelegramBot App

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"GO/CONSTS"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+
+	token := CONSTS.TOKEN // Your TOKEN here
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		if msg.Text == "!stop" {
+			log.Printf("%s", "Exit from Bot")
+			break
+		}
+		msg.Text = Reverse(msg.Text)
+		bot.Send(msg)
+	}
+
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
